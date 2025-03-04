274b571db528dcf482b1630ac72fd3291719e149
Tomas Mussi <tmussi@salesforce.com>
2025-03-04 17:03:29 -0300

    Small refactor

--- src/main/java/com/challenge/LargestPalindromicFinderApplication.java
+++ src/main/java/com/challenge/LargestPalindromicFinderApplication.java
@@ -10,14 +10,14 @@ public class LargestPalindromicFinderApplication {
 
     public static void main(String[] args) {
         if (args.length < 2) {
-            System.out.println("Usage: LargestPalindromicFinderApplication <number1> <number2>");
+            System.out.println("Usage: LargestPalindromicFinderApplication <number1> <number2> [-i -b]");
             return;
         }
         LargestPalindromicFinder finder = createFinder(args);
         int number1 = Integer.parseInt(args[0]);
         int number2 = Integer.parseInt(args[1]);
         int result = finder.findLargestPalindrome(number1, number2);
-        System.out.printf("The largest palindrome (using %s) is in the range %s and %s is: %s%n",
+        System.out.printf("The largest palindrome (using %s) in the range %s and %s is: %s%n",
                 finder.strategyName(),
                 number1,
                 number2,
