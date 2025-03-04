4c78dce2382a9cbeffc97be7e98ad77c780bd006
Tomas Mussi <tmussi@salesforce.com>
2025-03-04 14:45:17 -0300

    Implementing logic

--- src/main/java/com/challenge/LongestPalindromicFinderApplication.java
+++ src/main/java/com/challenge/LongestPalindromicFinderApplication.java
@@ -1,12 +1,23 @@
 package com.challenge;
 
+import com.challenge.core.BasicPalindromicFinder;
+import com.challenge.core.LongestPalindromicFinder;
+
 public class LongestPalindromicFinderApplication {
 
-
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        LongestPalindromicFinder finder = new BasicPalindromicFinder();
+        System.out.println(args.length);
         for (String s : args) {
             System.out.println(s);
         }
+        if (args.length != 2) {
+            System.out.println("Usage: LongestPalindromicFinderApplication <number1> <number2>");
+            return;
+        }
+        int number1 = Integer.parseInt(args[0]);
+        int number2 = Integer.parseInt(args[1]);
+        int result = finder.findLongestPalindrome(number1, number2);
+        System.out.println(String.format("The longest palindrome is in the range %s and %s is: %s", number1, number2, result));
     }
 }
--- src/main/java/com/challenge/core/BasicPalindromicFinder.java
+++ src/main/java/com/challenge/core/BasicPalindromicFinder.java
@@ -4,6 +4,27 @@ public class BasicPalindromicFinder implements LongestPalindromicFinder {
 
     @Override
     public int findLongestPalindrome(int number1, int number2) {
-        return 0;
+        int minimum = Math.min(number1, number2);
+        int maximum = Math.max(number1, number2);
+        for (int number = maximum; number >= minimum; number--) {
+            if (isPalindrome(number)) {
+                return number;
+            }
+        }
+        return -1;
+    }
+
+    private boolean isPalindrome(int number) {
+        String numberString = String.valueOf(number);
+        int left = 0;
+        int right = numberString.length() - 1;
+        while (left < right) {
+            if (numberString.charAt(left) != numberString.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
